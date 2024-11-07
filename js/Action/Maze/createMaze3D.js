89f9db47080d103e395bd70500613923726ccc8c
shooooooma415 <shoma20040415@icloud.com>
2024-11-07 15:38:55 +0900

    chenge:背景の色を変更

--- js/Action/Maze/createMaze3D.js
+++ js/Action/Maze/createMaze3D.js
@@ -6,6 +6,7 @@ export function createMaze3D(scene) {
     const walls = [];
     const wallGeometry = new THREE.BoxGeometry(1, 2, 1);
     const wallMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    const edgeMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
 
     for (let x = 0; x <= mazel; x++) {
         for (let y = 0; y <= mazel; y++) {
@@ -15,6 +16,11 @@ export function createMaze3D(scene) {
 
                 scene.add(wall);
                 walls.push(wall);
+
+                const edges = new THREE.EdgesGeometry(wallGeometry);  // エッジジオメトリを作成
+                const line = new THREE.LineSegments(edges, edgeMaterial);  // 輪郭線の作成
+                line.position.copy(wall.position);  // メッシュと同じ位置に配置
+                scene.add(line);
             }
         }
     }
